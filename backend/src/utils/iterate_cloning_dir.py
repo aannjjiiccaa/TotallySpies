673dea5983093cd5f0018175216184f671e6e7e1
Nemanja <nemanja.udovic@gmail.com>
2025-12-13 04:03:36 +0100

    added db, added iterating through db

--- backend/src/utils/iterate_cloning_dir.py
+++ backend/src/utils/iterate_cloning_dir.py
@@ -13,3 +13,26 @@ def iter_files(cloning_dir: str | Path):
             for file in files:
                 file_path = Path(root) / file
                 yield file_path
+
+
+def iter_chroma_entries(collection, batch_size=100):
+    offset = 0
+
+    while True:
+        batch = collection.get(
+            include=["documents", "metadatas", "ids"],
+            limit=batch_size,
+            offset=offset,
+        )
+
+        if not batch["ids"]:
+            break
+
+        for i in range(len(batch["ids"])):
+            yield {
+                "id": batch["ids"][i],
+                "document": batch["documents"][i],
+                "metadata": batch["metadatas"][i],
+            }
+
+        offset += batch_size
